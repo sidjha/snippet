5b0c668b40e66e8bc0a9ebd80e812f2faf876ee1
Siddharth Jha <sid@sidjha.com>
2014-09-06 20:57:06 -0400

    Added initial templates

--- app.py
+++ app.py
@@ -4,19 +4,34 @@ import mixpanel
 
 app = Flask(__name__)
 
+DEBUG_MODE = os.environ['SNIPPET_DEBUG']
+
 app.secret_key = os.environ['SNIPPET_APP_SECRET']
-debug_mode = os.environ['SNIPPET_DEBUG']
+
 
 MONGO_DB_NAME = os.environ['MONGO_DB_NAME']
 BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+MIXPANEL_TOKEN = os.environ['MIXPANEL_TOKEN']
 
 @app.route('/')
 def index():
-	return "Yo!"
+	if active_session() == True:
+		return render_template('logged_in.html')
+		# ANALYTICS TODO: trigger reloaded dashboard
+	else:
+		return render_template('landing.html')
+		# ANALYTICS TODO: trigger homepage visit
+
+def active_session():
+	if 'email' in session:
+		return True
+	else:
+		return False
+	
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', '5000'))
-	if debug_mode == 'true':
+	if DEBUG_MODE == 'true':
 		app.run(host='0.0.0.0', port=port, debug=True)
 	else:
 		app.run(host='0.0.0.0', port=port)
